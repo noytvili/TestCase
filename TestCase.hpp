d3aa998d7f1cae7197e11015499b131795d2c899
Doriya Spielman <33617822+Doriyaspielman@users.noreply.github.com>
2018-06-12 09:16:12 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -48,7 +48,7 @@ class TestCase{
         ostringstream new_os;
         new_os << a;
         if(new_os.str() != s1){
-               cerr << s << ": Failure in test #" << counter << ": string value should be " << a << " but is " << ans << "!" << endl;
+               cerr << s << ": Failure in test #" << counter << ": string value should be " << s1<< " but is " << a << "!" << endl;
           num_failure++;
         }
         return *this;    
